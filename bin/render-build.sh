8128eead4f1816bebbd4cf4b8978022200ca1ba5
Indrit Caca <cacaindrit81@gmail.com>
2024-03-09 00:41:36 +0100

    Changes

---
+++ bin/render-build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# exit on error
+# Test
+set -o errexit
+
+bundle install
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+bundle exec rake db:migrate
+bundle exec rake db:seed:replant DISABLE_DATABASE_ENVIRONMENT_CHECK=1
